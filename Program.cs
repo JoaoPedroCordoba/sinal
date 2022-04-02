165aac161a25bfbfcb6a47cb120aa349ce654953
JoaoPedroCordoba <jp.8pedro86@gmail.com>
2022-04-01 22:07:19 -0300

    finalizado exercicio

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿int n;
+
+Console.WriteLine("Digite um numero:");
+n = Convert.ToInt32(Console.ReadLine());
+
+bool n2;
+
+n2 = (n == 0);
+
+if (n2)
+{
+    Console.WriteLine("zero");
+}
+else  if (n < 0){
+        Console.WriteLine("negativo");
+    }
+    else{
+        Console.WriteLine("positivo");
+    }
